1de9155aea649bed0823869bb519959dd395d0f4
Александр Васильков <y915@yandex.ru>
2020-01-04 17:26:26 +0300

    Bean Lifecycle

---
+++ src/main/java/ru/voskhod/springdemoone/BeanLifeCycleDemoApp.java
@@ -0,0 +1,20 @@
+package ru.voskhod.springdemoone;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanLifeCycleDemoApp {
+    public static void main(String[] args) {
+        // load the spring configuration file
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("beanLifeCycle-applicationContext.xml");
+
+        // retrieve beans from the spring container
+        Coach theCoach = context.getBean("myCoach", Coach.class);
+
+        System.out.println(theCoach.getDailyWorkout());
+
+        // close context
+        context.close();
+
+    }
+}
--- src/main/java/ru/voskhod/springdemoone/TrackCoach.java
+++ src/main/java/ru/voskhod/springdemoone/TrackCoach.java
@@ -20,4 +20,14 @@ public class TrackCoach implements Coach {
     public String getDailyFortune() {
         return "Just do it! " + fortuneService.getFortune();
     }
+
+    // add an init method
+    public void doMyStartupStuff() {
+        System.out.println("TrackCoach: inside the init method");
+    }
+
+    // add a destroy method
+    public void doMyCleanupStuff() {
+        System.out.println("TrackCoach: inside the destroy method");
+    }
 }
