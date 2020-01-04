d808d742461095de2af9a8ec20747d214ff04ca1
Александр Васильков <y915@yandex.ru>
2020-01-04 17:12:02 +0300

    Bean Scopes

---
+++ src/main/java/ru/voskhod/springdemoone/BeanScopeDemoApp.java
@@ -0,0 +1,27 @@
+package ru.voskhod.springdemoone;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanScopeDemoApp {
+    public static void main(String[] args) {
+        // load the spring configuration file
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("beanScope-applicationContext.xml");
+
+        // retrieve beans from the spring container
+        Coach theCoach = context.getBean("myCoach", Coach.class);
+        Coach alphaCoach = context.getBean("myCoach", Coach.class);
+
+        // check if they are the same
+        boolean result = (theCoach == alphaCoach);
+
+        // print out the results
+        System.out.println("Pointing to the same object: " + result);
+        System.out.println("Memory location for theCoach: " + theCoach);
+        System.out.println("Memory location for alphaCoach: " + alphaCoach);
+
+        // close context
+        context.close();
+
+    }
+}
